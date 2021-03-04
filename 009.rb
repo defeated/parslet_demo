b288b8adf4bcf67ca995a571fc7dc06d4ad8c43e
Eddie Cianci <defeated2k4@gmail.com>
2021-03-04 10:24:58 -0500

    example of testing a rule in isolation

---
+++ 009.rb
@@ -0,0 +1,56 @@
+require 'bundler/inline'
+
+gemfile do
+  source 'https://rubygems.org'
+  gem 'parslet'
+  gem 'minitest'
+end
+
+class TxtParser < Parslet::Parser
+  root :record
+
+  rule :record do
+    (
+      bimi_data.as(:bimi_data) >>
+      location_data.as(:location_data)
+    ).as(:record)
+  end
+
+  rule :bimi_data do
+    str('v') >> item_separator >> match['[:alnum:]'].repeat.as(:v) >> field_separator
+  end
+
+  rule :location_data do
+    str('l') >> item_separator >> match['[:alnum:]'].repeat.as(:l) >> field_separator
+  end
+
+  # helpers
+  rule(:space?) { match[' \t'].maybe }
+  rule(:item_separator) { space? >> str('=') >> space? }
+  rule(:field_separator) { space? >> str(';').maybe >> space? }
+end
+
+require 'uri'
+class TxtTransform < Parslet::Transform
+  rule(v: simple(:v)) { v.to_s }
+  rule(l: simple(:l)) { URI.parse("https://example.com") }
+  rule(record: subtree(:record)) do
+    Struct.new("Record", :bimi_data, :location_data, keyword_init: true).new(record)
+  end
+end
+
+require 'minitest/autorun'
+require 'parslet/convenience'
+
+class TxtTest < Minitest::Test
+  def test_can_parse_bimi_data
+    parser = TxtParser.new.bimi_data
+    expected = { v: 'BIMI1' }
+
+    output = parser.parse_with_debug 'v=BIMI1;'
+    assert_equal output, expected
+
+    output = parser.parse_with_debug 'v = BIMI1 ;'
+    assert_equal output, expected
+  end
+end
